8b688e6c1adecb6807e410eaf2aaf6c3f4eae5d9
OsmanCanCinar <osmancancinar@gmail.com>
2024-06-07 18:34:36 +0300

    Add upload/download storage for avatar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_skeleton/pages/messages_page.dart';
@@ -6,6 +11,7 @@ import 'package:flutter_skeleton/pages/students_page.dart';
 import 'package:flutter_skeleton/pages/teachers_page.dart';
 import 'package:flutter_skeleton/providers/app_providers.dart';
 import 'package:flutter_skeleton/utilities/google_sign_in.dart';
+import 'package:image_picker/image_picker.dart';
 
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
@@ -43,13 +49,7 @@ class HomePage extends ConsumerWidget {
         child: ListView(
           padding: EdgeInsets.zero,
           children: [
-            DrawerHeader(
-              decoration: const BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text(
-                  'Welcome ${FirebaseAuth.instance.currentUser?.displayName}'),
-            ),
+            const DrawerWidget(),
             ListTile(
               title: const Text('Students'),
               onTap: () {
@@ -109,6 +109,103 @@ class HomePage extends ConsumerWidget {
                 child: Text("$newMessageCount Messages")),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class DrawerWidget extends StatefulWidget {
+  const DrawerWidget({
+    super.key,
+  });
+
+  @override
+  State<DrawerWidget> createState() => _DrawerWidgetState();
+}
+
+class _DrawerWidgetState extends State<DrawerWidget> {
+  Future<Uint8List?>? _picture;
+
+  @override
+  void initState() {
+    super.initState();
+    _picture = _downloadPicture();
+  }
+
+  Future<Uint8List?> _downloadPicture() async {
+    final uid = FirebaseAuth.instance.currentUser!.uid;
+    final documentSnapshot =
+        await FirebaseFirestore.instance.collection('users').doc(uid).get();
+    final userMap = documentSnapshot.data();
+
+    if (userMap == null) return null;
+
+    if (userMap.containsKey('pictureRef')) {
+      print("pictureRef - " + userMap['pictureRef']);
+      Uint8List? uInt8list = await FirebaseStorage.instance
+          .ref('pictures')
+          // .child('aEQDEOWtIsbb3q1mwjbI0XLvvss1.jpg')
+          .child(userMap['pictureRef'].replaceAll("pictures/", ""))
+          .getData();
+
+      if (uInt8list == null) return null;
+      return uInt8list;
+    }
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DrawerHeader(
+      decoration: const BoxDecoration(
+        color: Colors.blue,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Welcome ${FirebaseAuth.instance.currentUser?.displayName}'),
+          const SizedBox(height: 10),
+          InkWell(
+            onTap: () async {
+              print('avatar tapped');
+              XFile? xFile =
+                  await ImagePicker().pickImage(source: ImageSource.camera);
+              if (xFile == null) return;
+
+              final imagePath = xFile.path;
+              final uid = FirebaseAuth.instance.currentUser?.uid;
+              final pictureRef =
+                  FirebaseStorage.instance.ref('pictures').child("$uid.jpg");
+
+              await pictureRef.putFile(File(imagePath));
+
+              await FirebaseFirestore.instance
+                  .collection('users')
+                  .doc(uid)
+                  .update({
+                "pictureRef": pictureRef.fullPath,
+              });
+
+              setState(() {
+                _picture = _downloadPicture();
+              });
+            },
+            child: FutureBuilder<Uint8List?>(
+                future: _picture,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData && snapshot.data != null) {
+                    final picture = snapshot.data!;
+                    return CircleAvatar(
+                      backgroundImage: MemoryImage(picture),
+                    );
+                  } else {
+                    return const CircleAvatar(
+                      child: Text('OCD'),
+                    );
+                  }
+                }),
+          ),
+        ],
       ),
     );
   }
